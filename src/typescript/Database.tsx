99e940ade75359d6f3fe223031f42cc7201081b4
Tudor <pagu.tudor@gmail.com>
2023-03-06 16:39:20 +0200

    Database connection works

--- src/typescript/Database.tsx
+++ src/typescript/Database.tsx
@@ -5,10 +5,11 @@ import 'firebase/compat/auth';
 
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { DocumentData, updateDoc, doc } from 'firebase/firestore';
+import { useCollectionData, useDocument, useDocumentData } from 'react-firebase-hooks/firestore';
+import { DocumentData, updateDoc, doc, DocumentReference, getFirestore } from 'firebase/firestore';
 import { User } from 'firebase/auth';
 import secret from "../secret"
+import { useState } from 'react';
 
 const firebaseConfig = secret.firebaseConfig;
 firebase.initializeApp(firebaseConfig);
@@ -18,7 +19,7 @@ const firestore = firebase.firestore();
 
 async function add_to_collection<T extends DocumentData>(collection:string, object:T) {
     const collectionRef = firestore.collection(collection);
-    await collectionRef.add(object);
+    await collectionRef.doc(object.uid).set(object);
 }
 
 async function update_document<T extends DocumentData>(collection:string, id:string, data : T) {
@@ -26,7 +27,23 @@ async function update_document<T extends DocumentData>(collection:string, id:str
     await documentRef.update(data);
 }
 
+
+/**
+ * Takes a collection and a document path, and gives the value of that document, as well as a function to update it, and refreshes the component when
+ * the value changes. T is the type of the object that it receives from the database.
+*/
+function useDatabaseState<T>(collection:string,documentPath:string) : [T|undefined,<T extends DocumentData>(newVal:T)=>void] {
+    const query = firestore.collection(collection).doc(documentPath);
+    const [value,loadinmeMessages,error] = useDocumentData(query as any);
+    const updater = <T extends DocumentData>(newValue : T) => {
+        update_document<T>(collection,documentPath,newValue);
+    };
+
+    return [value as T|undefined,updater];
+}
+
 export {
     add_to_collection,
     update_document,
+    useDatabaseState,
 }
--- src/typescript/Game.tsx
+++ src/typescript/Game.tsx
@@ -1,31 +1,93 @@
-import { List } from 'immutable';
+import Immutable, { List, Record, RecordOf } from 'immutable';
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import BoardView from './BoardView';
 import './Database'
 import { add_to_collection } from './Database';
-import { Board, getEmptyBoard, updateBoard } from "./GameLogic"
+import { Board, BoardCell, getEmptyBoard, updateBoard } from "./GameLogic"
 import Player from './Player';
 import Cell from './PlayerEnum';
 import Position from './Position';
+import * as Database from "./Database";
+import { updateJsxFragment } from 'typescript';
 
+interface Game {
+    boardSize:number,
+    board:Board,
+    boardHistory:List<Board>,
+    currentPlayer:Player,
+}
+
+function gameToJSON({boardSize,board,boardHistory,currentPlayer}:Game) {
+    return {boardSize,board:JSON.stringify(board.toJS()),boardHistory:JSON.stringify(boardHistory.toJS())   ,currentPlayer,uid:"123"}
+}
 function Game() {
-    const {gameID} = useLoaderData() as {gameID:string};
-
+    const { gameID } = useLoaderData() as { gameID: string };
+    /*
+    
     const [boardSize, setBoardSize] = useState(13);
     const [board, setBoard] = useState(getEmptyBoard(boardSize));
     const [boardHistory, setBoardHistory] = useState(List<Board>());
 
     const [currentPlayer, setCurrentPlayer] = useState<Player>(Cell.Black);
 
+    Database.add_to_collection("games",{boardSize,board:JSON.stringify(board.toJS()),boardHistory:JSON.stringify(boardHistory.toJS())   ,currentPlayer,uid:"123"});
+    */
+
+    const [gameAux,setGame] = Database.useDatabaseState<any>("games",gameID);
+    if (gameAux === undefined) {
+        return <div>loading...</div>
+    }
+    function processDatabaseData(gameAux:any):Game {
+        function f(x:any) {
+            if (x === 0) {
+                return Cell.White;
+            } else if (x === 1) {
+                return Cell.Black;
+            } else {
+                return Cell.Empty; 
+            }
+        }
+        
+        function convertToBoard(v:any):Board {
+            return List<List<BoardCell>>(v.map((val:any)=>List<BoardCell>(val.map((x:any)=>f(x)))));
+        }
+        return {
+            boardSize: gameAux.boardSize,
+            board: convertToBoard(JSON.parse(gameAux.board)),
+            boardHistory:List<Board>(JSON.parse(gameAux.boardHistory).map((v:any)=>convertToBoard(v))),
+            currentPlayer: (gameAux.currentPlayer == 1) ? Cell.Black : Cell.White,
+
+        }
+    }
+
+    const GameRecord = Record(processDatabaseData(gameAux));
+    const game:RecordOf<Game> = GameRecord();
+    
+    function update<TProps extends object>(obj:RecordOf<TProps>, key : keyof TProps) {
+        const f = (newValue:typeof obj[typeof key]) => {
+            return obj.set(key, newValue);
+        }
+        return f;
+    }
+
+    const [board,setBoard] = [game.board,update(game,"board")];
+    const [boardHistory,setBoardHistory] = [game.boardHistory,update(game,"boardHistory")];
+
+    const [currentPlayer,setCurrentPlayer] = [game.currentPlayer,update(game,"currentPlayer")];
+
+
     const playerMove = (p: Position) => {
         const newBoard = updateBoard(board, p, currentPlayer, boardHistory);
         if (newBoard instanceof Error) {
             window.alert(newBoard.message);
         } else {
-            setBoard(newBoard);
-            setBoardHistory(boardHistory.push(board));
-            setCurrentPlayer((currentPlayer == Cell.White) ? Cell.Black : Cell.White);
+            const newGame = GameRecord({
+                board:newBoard,
+                boardHistory: boardHistory.push(board),
+                currentPlayer:(currentPlayer == Cell.White) ? Cell.Black : Cell.White,
+            })
+            setGame(gameToJSON(newGame));
         }
     }
 
