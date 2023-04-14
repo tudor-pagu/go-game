2a6a35f225a427f98f4f2bc4db57fe8e7c7622e9
Tudor <pagu.tudor@gmail.com>
2023-04-14 17:03:09 +0300

    firebase

---
+++ src/secret.js
@@ -0,0 +1,26 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhTNnNEi67tClbRqiPPVnO37jDpfADji8",
+    authDomain: "tudor-go.firebaseapp.com",
+    projectId: "tudor-go",
+    storageBucket: "tudor-go.appspot.com",
+    messagingSenderId: "595847368401",
+    appId: "1:595847368401:web:8d966c9b70a8493078fff0",
+    measurementId: "G-5667B47TYV"
+}
+
+const emulatorConfig = {
+    apiKey: "apiKey",
+    authDomain: "demo-something.firebaseapp.com",
+    databaseURL: "http://localhost:8080?ns=something",
+    projectId: "demo-something",
+    storageBucket: "demo-something.appspot.com",
+    messagingSenderId: "senderId",
+    appId: "appId",
+    measurementId: "measurementId",
+    emulatorHost: "localhost",
+    emulatorPort: "8080"
+};
+
+
+
+export default { firebaseConfig, emulatorConfig };
